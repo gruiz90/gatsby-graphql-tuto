77e0f5ec9d82d711d082c7761367f48e45d6caac
gruiz90 <germanruizravi@gmail.com>
2020-10-08 19:13:51 -0300

    Added private products feautre to auth users

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,61 +1,94 @@
 import React from 'react'
 import { graphql, Link, PageProps } from 'gatsby'
 import Img, { FluidObject } from 'gatsby-image'
+import netlifyIdentity from 'netlify-identity-widget'
 
 import Layout from '../components/layout'
 
+interface EdgeObject {
+    node: {
+        id: string
+        slug: string
+        name: string
+        price: number
+        private: boolean
+        image: {
+            fluid: FluidObject
+        }
+    }
+}
 interface DataProps {
     allContentfulProduct: {
-        edges: [
-            {
-                node: {
-                    id: string
-                    slug: string
-                    name: string
-                    price: number
-                    image: {
-                        fluid: FluidObject
-                    }
-                }
-            }
-        ]
+        edges: EdgeObject[]
     }
 }
 type DataPageProps = PageProps<DataProps>
 
-const Products = ({ data: { allContentfulProduct } }: DataPageProps) => (
-    <Layout>
-        <div>
-            {/* Products List */}
-            {allContentfulProduct.edges.map(({ node: product }) => (
-                <div key={product.id}>
-                    <h2>Garb Products</h2>
-                    <Link
-                        to={`/products/${product.slug}`}
-                        style={{ textDecoration: 'none', color: 'darkblue' }}
-                    >
-                        <h3>
-                            {product.name} -{' '}
-                            <span
+interface MyState {
+    products: EdgeObject[]
+}
+
+class Products extends React.Component<DataPageProps, MyState> {
+    state: MyState = {
+        products: [],
+    }
+
+    componentDidMount() {
+        this.getProducts()
+        netlifyIdentity.on('login', user => this.getProducts(user))
+        netlifyIdentity.on('logout', () => this.getProducts())
+    }
+
+    getProducts = (user?: netlifyIdentity.User) => {
+        console.log('Current User', user)
+        const allProducts = this.props.data.allContentfulProduct.edges
+        const products =
+            netlifyIdentity.currentUser() !== null
+                ? allProducts
+                : allProducts.filter(({ node: product }) => !product.private)
+        this.setState({ products })
+    }
+
+    render() {
+        const { products } = this.state
+        return (
+            <Layout>
+                <div>
+                    {/* Products List */}
+                    {products.map(({ node: product }) => (
+                        <div key={product.id}>
+                            <h2>Garb Products</h2>
+                            <Link
+                                to={`/products/${product.slug}`}
                                 style={{
-                                    fontSize: '1.2rem',
-                                    fontWeight: 300,
-                                    color: '#f60',
+                                    textDecoration: 'none',
+                                    color: 'darkblue',
                                 }}
                             >
-                                ${product.price}
-                            </span>
-                        </h3>
-                    </Link>
-                    <Img
-                        style={{ maxWidth: 400 }}
-                        fluid={product.image.fluid}
-                    />
+                                <h3>
+                                    {product.name} -{' '}
+                                    <span
+                                        style={{
+                                            fontSize: '1.2rem',
+                                            fontWeight: 300,
+                                            color: '#f60',
+                                        }}
+                                    >
+                                        ${product.price}
+                                    </span>
+                                </h3>
+                            </Link>
+                            <Img
+                                style={{ maxWidth: 400 }}
+                                fluid={product.image.fluid}
+                            />
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-    </Layout>
-)
+            </Layout>
+        )
+    }
+}
 
 export const query = graphql`
     query {
@@ -66,6 +99,7 @@ export const query = graphql`
                     slug
                     name
                     price
+                    private
                     image {
                         fluid(maxWidth: 400) {
                             ...GatsbyContentfulFluid
